b6de4fb096d45abb633d3e40f045bcbb25982623
Jbohacek <jbohacek@email.cz>
2025-04-14 13:45:32 +0200

    Broadcast zacatek

---
+++ BluChat.Core/Messages/MessageTypes/ClientBroadcastMessage.cs
@@ -0,0 +1,17 @@
+﻿using BluChat.Core.Messages.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BluChat.Core.Messages.MessageTypes
+{
+    class ClientBroadcastMessage : MessageBaseClient
+    {
+        public override void MessangeHandler(MessageClientManager clientManager)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
